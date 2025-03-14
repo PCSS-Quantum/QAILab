0bc83b2c2fabf5c9a88de463cfd1956ddedcb914
Jakub Jagla <jakub.jagla01@gmail.com>
2025-03-14 17:38:50 +0100

    circuit_builder WIP

---
+++ qlearning/circuit/trainable/entanglers.py
@@ -0,0 +1,13 @@
+from qiskit import QuantumCircuit
+
+from qlearning.circuit.base import EntanglingBlock
+
+
+class CXEntangler(EntanglingBlock):
+    def __init__(self, num_qubits) -> None:
+        super().__init__(num_qubits, 'CXEntangler')
+        self._circuit = QuantumCircuit(num_qubits)
+        for i in range(1, num_qubits):
+            self._circuit.cx(i-1, i)
+
+        self._circuit.cx(num_qubits-1, 0)
