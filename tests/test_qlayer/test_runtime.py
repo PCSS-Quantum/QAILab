222e52510056cf9cbc3303d8c86bcb2a4c74e211
Dawid64 <dawid.siera@gmail.com>
2025-03-19 16:51:08 +0100

    Added tests for hybrid networks

--- tests/test_qlayer/test_runtime.py
+++ tests/test_qlayer/test_runtime.py
@@ -16,26 +16,52 @@ def build_circuit() -> qiskit.QuantumCircuit:
     return circuit
 
 
-class QuantumModel(nn.Module):
-    """ QModel """
-
-    def __init__(self):
-        super().__init__()
-        self.net = torch.nn.Sequential(
-            QLayer(build_circuit()),
-        )
-
-    def forward(self, x):
-        """ Forward pass """
-        return self.net(x)
-
-
 def test_runtime():
     """ Runtime test """
+    class QuantumModel(nn.Module):
+        """ QModel """
+
+        def __init__(self):
+            super().__init__()
+            self.net = torch.nn.Sequential(
+                QLayer(build_circuit()),
+            )
+
+        def forward(self, x):
+            """ Forward pass """
+            return self.net(x)
     quantum_model = QuantumModel()
     loss_fn = nn.MSELoss()
     desired_result = torch.Tensor([1])
-    predictions = quantum_model(torch.Tensor([0.111111, .3]))
+    test_input = torch.Tensor([-0.11, .3, ])
+    predictions = quantum_model(test_input)
+    assert isinstance(predictions, torch.Tensor)
+    loss = loss_fn(predictions, desired_result)
+    assert isinstance(loss, torch.Tensor)
+
+
+def test_integration():
+    """ Integration with classical layers test """
+    class QuantumModel(nn.Module):
+        """ Hybrid model """
+
+        def __init__(self):
+            super().__init__()
+            self.net = torch.nn.Sequential(
+                nn.Linear(4, 2),
+                QLayer(build_circuit()),
+                nn.Linear(1, 1),
+            )
+
+        def forward(self, x):
+            """ Forward pass """
+            return self.net(x)
+
+    quantum_model = QuantumModel()
+    loss_fn = nn.MSELoss()
+    desired_result = torch.Tensor([1])
+    test_input = torch.Tensor([-0.111111, .3, 1, 1])
+    predictions = quantum_model(test_input)
     assert isinstance(predictions, torch.Tensor)
     loss = loss_fn(predictions, desired_result)
     assert isinstance(loss, torch.Tensor)
