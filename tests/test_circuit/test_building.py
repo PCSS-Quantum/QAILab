71637b24ed7428b37fef07242181ea7d50fcd4e5
Jakub Jagla <jakub.jagla01@gmail.com>
2025-03-19 12:02:42 +0100

    add possibility of state initialization instead of encoding blocks, multiple encoding blocks, auxiliary qubits. Make blocks parametrizable

--- tests/test_circuit/test_building.py
+++ tests/test_circuit/test_building.py
@@ -6,15 +6,13 @@ from qiskit.primitives import Sampler
 
 from qlearning.circuit.circuit_builder import build_circuit
 
+from qlearning.circuit.base import CircuitBlock
 from qlearning.circuit.layer_blocks import CXEntangler
 from qlearning.circuit.encoding_blocks import RotationalEncoder
 
 
-from qlearning.circuit.measurement_blocks import FirstQubitMeasurement, AllQubitMeasurement
-
-
 def test_builder():
-    res = build_circuit(4, [RotationalEncoder('y')], [RotationalEncoder('x'), CXEntangler()],  FirstQubitMeasurement())
+    res = build_circuit(4, [RotationalEncoder('y')], [RotationalEncoder('x'), CXEntangler()], [0])
     assert isinstance(res, tuple)
     assert len(res) == 3
 
@@ -32,8 +30,28 @@ def test_builder():
     assert sum(dict(c.count_ops()).values()) == 4  # check if all blocks get added as gates
 
 
+class AuxBlock(CircuitBlock):
+    def _build_circuit(self, num_qubits: int) -> QuantumCircuit:
+        c = QuantumCircuit(num_qubits + 1)
+        for i in range(num_qubits+1):
+            c.x(i)
+        return c
+
+
+def test_block_with_aux_qubits():
+    """Test if blocks using more qubits than num_qubits get added correctly"""
+    c = QuantumCircuit(2)
+    b = AuxBlock(name="e")
+    b.add_to_circuit(c)
+
+    b2 = AuxBlock(name="e")
+    b2.add_to_circuit(c, [0, 1])
+
+    assert c.num_qubits == 4
+
+
 def test_first_qubit_measurements():
-    c, x, w = build_circuit(3, [RotationalEncoder('z')], [RotationalEncoder('y'), CXEntangler()], FirstQubitMeasurement())
+    c, x, w = build_circuit(3, [RotationalEncoder('z')], [RotationalEncoder('y'), CXEntangler()], [0])
     res = Sampler().run(
         c.assign_parameters(
             {
@@ -47,7 +65,7 @@ def test_first_qubit_measurements():
 
 
 def test_all_qubit_measurement():
-    c, x, w = build_circuit(3, [RotationalEncoder('x')], [RotationalEncoder('z'), CXEntangler()], AllQubitMeasurement())
+    c, x, w = build_circuit(3, [RotationalEncoder('x')], [RotationalEncoder('z'), CXEntangler()])
     res = Sampler().run(
         c.assign_parameters(
             {
@@ -58,3 +76,11 @@ def test_all_qubit_measurement():
     )
 
     assert len(res.result().quasi_dists[0]) == 2**3
+
+
+def test_param_block_can_be_added_twice():
+    """Test if a block added twice generates only one instance of parameter vectors to set later"""
+    b = RotationalEncoder('x')
+    _, x, _ = build_circuit(3, [b, b])
+
+    assert x == [b.parameter_vector]
