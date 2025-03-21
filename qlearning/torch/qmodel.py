7d51c1db687eed15e0b6cee8e2ce783d4bc91bb1
Anatol Kaczmarek <92335424+Tole-k@users.noreply.github.com>
2025-03-21 14:11:59 +0100

    add set_params

--- qlearning/torch/qmodel.py
+++ qlearning/torch/qmodel.py
@@ -19,6 +19,7 @@ available_optimizers: dict[str, type[Optimizer]] = {opt.__name__.lower(): opt fo
 class QModel(Module, BaseEstimator):
     """ Quantum model class """
     layers: ModuleList
+    optimizer_type: type[Optimizer]
     optimizer: Optimizer
     loss: Callable
     batch_size: int
@@ -29,7 +30,7 @@ class QModel(Module, BaseEstimator):
     def __init__(
         self,
         layers: list[Module],
-        optimizer: type[Optimizer] | str | None = None,
+        optimizer_type: type[Optimizer] | str | None = None,
         loss: Callable | None = None,
         batch_size: int = 1,
         epochs: int = 1,
@@ -39,15 +40,16 @@ class QModel(Module, BaseEstimator):
     ):
         super().__init__()
         self.layers = ModuleList(layers)
-        if optimizer is None:
-            optimizer = optim.AdamW
-        if isinstance(optimizer, str):
+        if optimizer_type is None:
+            optimizer_type = optim.AdamW
+        if isinstance(optimizer_type, str):
             try:
-                optimizer = available_optimizers[optimizer]
+                optimizer_type = available_optimizers[optimizer_type]
             except ValueError as e:
                 raise ValueError(
-                    f"Unknown optimizer: {optimizer}. Available optimizers are: {list(available_optimizers.keys())}") from e
-        self.optimizer = optimizer(self.parameters())
+                    f"Unknown optimizer: {optimizer_type}. Available optimizers are: {list(available_optimizers.keys())}") from e
+        self.optimizer_type = optimizer_type
+        self.optimizer = optimizer_type(self.parameters())
         if loss is None:
             loss = MSELoss()
         self.loss = loss
@@ -114,7 +116,7 @@ class QModel(Module, BaseEstimator):
             self.eval()
             with torch.inference_mode():
                 valid_loss = self._validate_one_epoch(validation_loader)
-            pbar.set_postfix(loss=valid_loss, epoch=epoch+1)
+            pbar.set_postfix(loss=valid_loss, epoch=epoch + 1)
 
     def _train_one_epoch(self, train_loader: DataLoader) -> np.floating:
         losses = []
@@ -127,7 +129,7 @@ class QModel(Module, BaseEstimator):
             loss.backward()
             self.optimizer.step()
             losses.append(loss.item())
-            pbar.set_postfix(loss=loss.item(), batch=batch+1)
+            pbar.set_postfix(loss=loss.item(), batch=batch + 1)
 
         return np.mean(losses)
 
@@ -139,7 +141,7 @@ class QModel(Module, BaseEstimator):
             outputs = self(x)
             loss = self.loss(outputs, y)
             losses.append(loss.item())
-            pbar.set_postfix(loss=loss.item(), batch=batch+1)
+            pbar.set_postfix(loss=loss.item(), batch=batch + 1)
 
         return np.mean(losses)
 
@@ -151,3 +153,23 @@ class QModel(Module, BaseEstimator):
         self.eval()
         with torch.inference_mode():
             return self(x).cpu()
+
+    def set_params(self, **params):
+        if not params:
+            return self
+        valid_params = self.get_params(deep=False)
+        for key, value in params.items():
+            if key not in valid_params:
+                raise ValueError(
+                    f"Invalid parameter {key!r} for estimator {self}. "
+                    f"Valid parameters are: {valid_params.keys()!r}."
+                )
+            if key == "device":
+                self.device = value
+                self.to(self.device)
+            if key == "optimizer_type":
+                self.optimizer_type = value
+                self.optimizer = self.optimizer_type(self.parameters())
+            if key == "layers":
+                self.layers = ModuleList(value)
+        return self
--- tests/test_qmodel.py
+++ tests/test_qmodel.py
@@ -1,5 +1,6 @@
 """ Base tests for QModel """
-from torch.nn import Linear, ReLU, MSELoss
+from torch.nn import Linear, ReLU, MSELoss, Conv2d
+from torch import optim
 from torch.optim import Adam
 import numpy as np
 
@@ -18,3 +19,15 @@ def test_runtime():
     assert isinstance(q_model, QModel)
     assert len(q_model.predict(np.random.rand(10, 10)) == 10)
     assert len(q_model.fit_predict(np.random.rand(100, 10), np.random.rand(100, 1))) == 100
+
+
+def test_changing_params():
+    """ Testing params test """
+    q_model = QModel([Linear(10, 1)])
+    assert isinstance(q_model.get_params(), dict)
+    q_model = q_model.fit(np.random.rand(100, 10), np.random.rand(100, 1))
+    assert len(q_model.predict(np.random.rand(10, 10)) == 10)
+    q_model.set_params(layers=[Conv2d(3, 1, 3), ReLU()], optimizer_type=optim.Adagrad)
+    assert isinstance(q_model.get_params(), dict)
+    q_model = q_model.fit(np.random.rand(100, 3, 100, 100), np.random.rand(100, 1, 98, 98))
+    assert len(q_model.predict(np.random.rand(10, 3, 100, 100)) == 10)
