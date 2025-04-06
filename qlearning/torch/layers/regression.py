3190c2edb4f09d0c30b6bcd15d3bfa0e532faa56
Jakub Jagla <jakub.jagla01@gmail.com>
2025-04-06 22:49:27 +0200

    add rescaling to expected value qlayer

--- qlearning/torch/layers/regression.py
+++ qlearning/torch/layers/regression.py
@@ -13,9 +13,36 @@ class ExpectedValueQLayer(QLayer):
     The value is a floating point number in range <0, 2^num_measured_qubits - 1>
     """
 
-    def __init__(self, circuit: QuantumCircuit, *, backend: QiskitBackend | None = None, shots: int = 1024) -> None:
+    def __init__(
+        self,
+        circuit: QuantumCircuit,
+        *,
+        backend: QiskitBackend | None = None,
+        shots: int = 1024,
+        rescale_output: tuple[float, float] | None = None
+    ) -> None:
+        """
+        Args:
+            circuit (QuantumCircuit): Circuit to run.
+            backend (QiskitBackend | None, optional): Backend to use. If None a local QiskitBackend is created. Defaults to None.
+            shots (int, optional): Number of times to sample the circuit. Defaults to 1024.
+            rescale_output (tuple[float,float] | None, optional):
+            Tuple of (low, high) representing a range to rescale output to.
+            If None the output will be in range <0, 2^num_measured_qubits - 1>. Defaults to None.
+        """
         super().__init__(circuit, backend=backend, shots=shots)
+        self._max_expected_out_value = self.out_features - 1
         self.out_features = 1
+        self._rescale_output_range = rescale_output
+
+    def _rescale_out(self, x: torch.Tensor):
+        if self._rescale_output_range is None:
+            return x
+
+        out_min, out_max = self._rescale_output_range
+        x_zero_one = x / self._max_expected_out_value
+        x_scale = x_zero_one * (out_max - out_min) + out_min
+        return x_scale
 
     def forward(self, input_tensor: torch.Tensor) -> torch.Tensor:
         out_distribution = super().forward(input_tensor)
@@ -33,7 +60,8 @@ class ExpectedValueQLayer(QLayer):
             return torch.sum(out_distribution * values)
         # Batched input
         values = torch.stack([make_vals(out_distribution.shape[1])] * out_distribution.shape[0])
-        return torch.sum(out_distribution * values, dim=-1)
+        summed = torch.sum(out_distribution * values, dim=-1)
+        return self._rescale_out(summed)
 
 
 class ArgmaxQLayer(QLayer):
