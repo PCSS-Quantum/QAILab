3e08f9025dd3e7f7da766818793f49a1e651d187
Jakub Jagla <jakub.jagla01@gmail.com>
2025-04-09 12:09:02 +0200

    update __init__ of qlauncher

--- qlearning/qlauncher/__init__.py
+++ qlearning/qlauncher/__init__.py
@@ -2,6 +2,5 @@
 from .passes import ForwardPass, BackwardPass
 from .problem import CircuitProblem
 from .formatter import _CircuitForwardFormatter
-from .aer_backend import AerBackend
 
-__all__ = ['ForwardPass', 'BackwardPass', 'CircuitProblem', 'AerBackend', '_CircuitForwardFormatter']
+__all__ = ['ForwardPass', 'BackwardPass', 'CircuitProblem', '_CircuitForwardFormatter']
